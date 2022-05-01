4ddb23eab12466672f6667820f37a1fefac62100
Mickmav <mimav@hotmail.be>
2022-05-02 00:06:16 +0200

    Query 5, 7 correction, 1 problem remain in 7 somme company appear many times with different average_passenger

--- q5.sql
+++ q5.sql
@@ -1,16 +1,15 @@
-select distinct v.distance, v.heurearrivée :: date, c.nom
+select distinct a.average_dist, (v.heurearrivée :: date) as date, c.nom
 from vol v, company c,
-     (select   avion.compagnieid, avion.id,
+     (select  distinct avion.compagnieid, avion.id,
                 to_char(
                     AVG(distance),'99999999999999999D99'
-                    )AS average_amount
+                    )AS average_dist
                 from   vol
                 INNER JOIN avion ON avion.id = vol.avionid
                 group by
-                        avion.id, vol.heurearrivée::date
+                        avion.id
                 order by
                         id) as a
-
 where a.compagnieid=c.id and a.id= v.avionid and
       (c.nom = 'ADVANCED AIR, LLC' or c.nom = 'ABX Air Inc');
 
--- q7.sql
+++ q7.sql
@@ -1,16 +1,15 @@
-
 select distinct c.nom, sol.average_passenger
+
 from  ( select distinct  avion.compagnieid,
                 to_char(
                     AVG(passengers),'99999999999999999D99'
                     )AS average_passenger
-        from   (SELECT distinct r.volid, COUNT('*') AS passengers
-                from réservation r
-                group by r.volid
-                having count('*')<20) as vp,
-                company c, vol
+        from   (SELECT distinct volid, count('*') as passengers
+                from réservation
+                group by volid
+                having count('*')<20) as vp, vol
         INNER JOIN avion ON avion.id = vol.avionid
-        where vol.id = vp.volid
+        where vp.volid = vol.id
         group by
             avion.compagnieid
         order by
@@ -18,3 +17,5 @@ from  ( select distinct  avion.compagnieid,
 where c.id=sol.compagnieid
 order by sol.average_passenger
 ;
+select distinct nom
+from company
