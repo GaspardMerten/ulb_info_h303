a8a327d5383b0e669af2b014efecf38ef139cb77
Gaspard Merten <gaspard.mp.work@gmail.com>
2022-05-08 17:57:06 +0200

    Adding table creation

--- q9.sql
+++ q9.sql
@@ -1,6 +1,9 @@
-CREATE TABLE experts (
-                            piloteId uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-                            FOREIGN KEY (piloteid ) REFERENCES pilote.id
+CREATE TABLE IF NOT EXISTS expert
+(
+    piloteId uuid DEFAULT uuid_generate_v4() NOT NULL PRIMARY KEY
+        CONSTRAINT fk_id
+            REFERENCES utilisateur,
 
-
+    date     TIMESTAMP                       NOT NULL,
+    nouveau  BOOLEAN                         NOT NULL
 );
