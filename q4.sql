6fca20c3f266435b906df70db5fe904db252902a
Gaspard Merten <gaspard.mp.work@gmail.com>
2022-05-14 15:56:18 +0200

    Fixinf Q4 -> excluding pilot with no preious flight record

--- q4.sql
+++ q4.sql
@@ -1,10 +1,11 @@
-SELECT u.nom
+SELECT u.prénom
 FROM pilote
          INNER JOIN utilisateur u ON u.id = pilote.id
 WHERE NOT EXISTS(
-        SELECT pilote.id
-        FROM vol,
-             aviondefret
-        WHERE vol.piloteid = pilote.id
-          AND vol.avionid = aviondefret.id
-    );
+        SELECT *
+        FROM vol
+                 LEFT JOIN aviondeligne adl ON adl.id = vol.avionid
+        WHERE vol.piloteid = u.id
+          AND adl.id IS NULL
+    )
+  AND EXISTS(SELECT * FROM vol WHERE vol.piloteid = u.id);
