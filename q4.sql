744734776aaaabb0daab7017c1a92b4ae1b95138
Mickmav <mimav@hotmail.be>
2022-05-14 18:45:34 +0200

    q4 and q6 updated

--- q4.sql
+++ q4.sql
@@ -1,11 +1,10 @@
-SELECT u.prénom
+SELECT distinct u.prénom, u.nom
 FROM pilote
          INNER JOIN utilisateur u ON u.id = pilote.id
+         inner join vol v on v.piloteid=u.id
 WHERE NOT EXISTS(
         SELECT *
         FROM vol
                  LEFT JOIN aviondeligne adl ON adl.id = vol.avionid
         WHERE vol.piloteid = u.id
-          AND adl.id IS NULL
-    )
-  AND EXISTS(SELECT * FROM vol WHERE vol.piloteid = u.id);
+          AND adl.id IS NULL);
--- q6.sql
+++ q6.sql
@@ -1,9 +1,12 @@
-SELECT volallee.id, volretour.id
-FROM vol volallee
-         INNER JOIN aviondeligne a ON a.id = volallee.avionid
-         INNER JOIN vol volretour ON volretour.aéroportarrivéecode = volallee.aéroportdépartcode AND
-                                     volretour.aéroportdépartcode = volallee.aéroportarrivéecode
-WHERE EXTRACT(HOUR FROM volallee.heuredépart) >= 7
-  AND EXTRACT(HOUR FROM (volallee.heurearrivée - volretour.heuredépart)) >= 7
-  AND volretour.heuredépart::DATE = volallee.heurearrivée::DATE;
-
+select distinct etat.nom
+from aéroport, etat, (
+    SELECT volallee.aéroportarrivéecode
+    FROM vol volallee
+             INNER JOIN aviondeligne a ON a.id = volallee.avionid
+             INNER JOIN vol volretour ON volretour.aéroportarrivéecode = volallee.aéroportdépartcode AND
+                                         volretour.aéroportdépartcode = volallee.aéroportarrivéecode
+    WHERE EXTRACT(HOUR FROM volallee.heuredépart) >= 7
+      AND EXTRACT(HOUR FROM (volretour.heuredépart - volallee.heurearrivée )) >= 7
+      AND volretour.heuredépart::DATE = volallee.heuredépart::DATE) as volok
+where aéroport.etatcode=etat.code and volok.aéroportarrivéecode=aéroport.code
+;
