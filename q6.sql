b790731eb890bba2b4dbd3b142d4538942c59b30
Mickmav <mimav@hotmail.be>
2022-05-14 19:03:01 +0200

    q6 updated

--- q6.sql
+++ q6.sql
@@ -1,6 +1,6 @@
-select distinct etat.nom
-from aéroport, etat, (
-    SELECT volallee.aéroportarrivéecode
+select distinct e1.nom, e2.nom
+from aéroport a1,aéroport a2, etat e1, etat e2, (
+    SELECT volallee.aéroportarrivéecode, volallee.aéroportdépartcode
     FROM vol volallee
              INNER JOIN aviondeligne a ON a.id = volallee.avionid
              INNER JOIN vol volretour ON volretour.aéroportarrivéecode = volallee.aéroportdépartcode AND
@@ -8,5 +8,6 @@ from aéroport, etat, (
     WHERE EXTRACT(HOUR FROM volallee.heuredépart) >= 7
       AND EXTRACT(HOUR FROM (volretour.heuredépart - volallee.heurearrivée )) >= 7
       AND volretour.heuredépart::DATE = volallee.heuredépart::DATE) as volok
-where aéroport.etatcode=etat.code and volok.aéroportarrivéecode=aéroport.code
+where a1.etatcode=e1.code and volok.aéroportarrivéecode=a1.code and
+      a2.etatcode=e2.code and volok.aéroportdépartcode=a2.code
 ;
